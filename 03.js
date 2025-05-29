684b3356da24fdc3ae105b7e81355080b4ef2997
analauracoan <anacoan1@gmail.com>
2025-05-29 10:10:30 -0300

    feat(q03): responde questão sobre esse em algoritmo fornecido

---
+++ 03.js
@@ -0,0 +1,10 @@
+// QUESTÃO 03
+// Autor(a): Ana Laura Coan
+// Data e Hora de Criação: 29/05/2025, 10:06
+// Data e Hora da Última Alteração: 00/00/0000, 00:00
+
+// ENUNCIADO
+// Este algoritmo possui um erro. QUe erro é esse? 
+
+// RESPOSTA
+// b) Loop infinito
