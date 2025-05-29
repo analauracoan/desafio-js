cb922543ad123efae5b1de9a33ff1b96dbd1141b
analauracoan <anacoan1@gmail.com>
2025-05-29 11:37:41 -0300

    refactor(q07): ajusta função para exportação e retorno

--- 07.js
+++ 07.js
@@ -1,20 +1,20 @@
 // QUESTÃO 07
 // Autor(a): Ana Laura Coan
 // Data e Hora de Criação: 29/05/2025, 10:38
-// Data e Hora da Última Alteração: 00/00/0000, 00:00
+// Data e Hora da Última Alteração: 29/05/2025, 11:36
 
 // ENUNCIADO
 // Faça um algoritimo que, dado um array de inteiros, e um número objetivo, retorne os index de dois números
 // dentro desse array, que se adicionados resultam nesse objetivo.
 
-function encontraSoma(array, objetivo) {
+export function encontraSomaDeObjetivo(array, objetivo) {
     const mapa = {};
 
     for (let i = 0; i < array.length; i++) {
         const complemento = objetivo - array[i];
 
         if (mapa.hasOwnProperty(complemento)) {
-            console.log([mapa[complemento], i]);
+            return ([mapa[complemento], i]);
         }
         mapa[array[i]] = i;
     }
