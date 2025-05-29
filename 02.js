951e6b011ed8497723ae3dbe505e37f0c4188edd
analauracoan <anacoan1@gmail.com>
2025-05-29 11:17:53 -0300

    refactor(q02): ajusta função para exportação e retorno

--- 02.js
+++ 02.js
@@ -1,17 +1,17 @@
 // QUESTÃO 02
 // Autor(a): Ana Laura Coan
 // Data e Hora de Criação: 29/05/2025, 09:57
-// Data e Hora da Última Alteração: 00/00/0000, 00:00
+// Data e Hora da Última Alteração: 29/05/2025, 11:11
 
 // ENUNCIADO
 // Faça uma função que receba um array de números inteiros e um número inteiro. A função deverá retornar
 // se o número passado está informado no array e se sim, em que posição.
 
-function retornaArray(array, pesquisa) {
+export function encontraNumeroEPosicao(array, pesquisa) {
   const posicao = array.indexOf(pesquisa);
   if (posicao > 0) {
-    console.log(`O número está presente na posição: ${posicao}`);
+    return "O número está presente na posição: ${posicao}";
   } else {
-    console.log('o número não existe no array');
+    return "o número não existe no array";
   }
 }
