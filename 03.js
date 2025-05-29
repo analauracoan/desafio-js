4c3e97786caeccb6d18ac6c0f293fdc2e4a76b67
analauracoan <anacoan1@gmail.com>
2025-05-29 11:25:37 -0300

    refactor(q03): ajusta função para exportação e retorno

--- 03.js
+++ 03.js
@@ -1,10 +1,12 @@
 // QUESTÃO 03
 // Autor(a): Ana Laura Coan
 // Data e Hora de Criação: 29/05/2025, 10:06
-// Data e Hora da Última Alteração: 00/00/0000, 00:00
+// Data e Hora da Última Alteração: 29/05/2025, 11:20
 
 // ENUNCIADO
-// Este algoritmo possui um erro. QUe erro é esse? 
+// Este algoritmo possui um erro. Que erro é esse? 
 
 // RESPOSTA
-// b) Loop infinito
+export function alternativaCorreta() {
+    return "Alternativa b) Loop infinito";
+}
