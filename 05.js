299ee43ecd861076dda2753f50dc61d6f5a5ac31
analauracoan <anacoan1@gmail.com>
2025-05-29 10:30:52 -0300

    feat(q05): implementa a classe Pilha com métodos adicionar e remover

---
+++ 05.js
@@ -0,0 +1,24 @@
+// QUESTÃO 05
+// Autor(a): Ana Laura Coan
+// Data e Hora de Criação: 29/05/2025, 10:24
+// Data e Hora da Última Alteração: 00/00/0000, 00:00
+
+// ENUNCIADO
+// Crie uma classe chamada Pilha. Essa classe deverá ter um método chamado adicionar que deverá receber
+// um nome e adicionar à Pilha e outro método chamado remover que deverá remover o último nome Pilha. A
+// estrutura de dados deve funcionar como Pilha, o último registro a ser adicionado deve ser o primeiro a ser
+// removido.
+
+class Pilha {
+    constructor() {
+        this.itens = [];
+    }
+
+    adicionar(nome) {
+        this.itens.push(nome);
+    }
+
+    remover() {
+        this.itens.pop();
+    }
+}
