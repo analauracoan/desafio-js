c7dcfa118a36dcf9ab26742e75636f8dda8feea0
analauracoan <anacoan1@gmail.com>
2025-05-29 11:35:18 -0300

    refactor(q05): ajusta classe para exportação

--- 05.js
+++ 05.js
@@ -1,7 +1,7 @@
 // QUESTÃO 05
 // Autor(a): Ana Laura Coan
 // Data e Hora de Criação: 29/05/2025, 10:24
-// Data e Hora da Última Alteração: 00/00/0000, 00:00
+// Data e Hora da Última Alteração: 29/05/2025, 11:34
 
 // ENUNCIADO
 // Crie uma classe chamada Pilha. Essa classe deverá ter um método chamado adicionar que deverá receber
@@ -9,7 +9,7 @@
 // estrutura de dados deve funcionar como Pilha, o último registro a ser adicionado deve ser o primeiro a ser
 // removido.
 
-class Pilha {
+export class Pilha {
     constructor() {
         this.itens = [];
     }
